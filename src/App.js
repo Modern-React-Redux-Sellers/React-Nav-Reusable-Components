9cbed0cadf2c1b340dd7f85ddf7c07b4f29e058c
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2024-01-10 21:18:44 -0600

    completed button component, covered use of ...rest object

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import Button from "./components/Button";
+import {GoBell, GoCloud, GoDesktopDownload, GoDatabase} from "react-icons/go";
 
 const App = () => {
+
     return (
         <div>
             App.js
             <div>
-                <Button>children prop</Button>
+                <Button primary >
+                    <GoBell />
+                    children prop
+                </Button>
             </div>
             <div>
-                <Button>Click me</Button>
+                <Button warning ><GoCloud/>Click me</Button>
             </div>
             <div>
-                <Button>Ayo</Button>
+                <Button success outline ><GoDesktopDownload/>Ayo</Button>
+            </div>
+            <div>
+                <Button danger rounded outline><GoDatabase/>Click me</Button>
             </div>
         </div>
     )
