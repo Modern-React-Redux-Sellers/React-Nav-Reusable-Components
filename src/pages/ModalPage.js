37c5ba01801c41b1a6a2ac24fab572db7ce22da0
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2024-02-23 14:40:20 -0600

    completed closing modal function, scrolling fix, and position fix of modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,18 +1,31 @@
 import {useState} from "react";
 import Modal from "../components/Modal";
 import Button from "../components/Button";
+import modal from "../components/Modal";
 
 const ModalPage = () => {
     const [showModal, setShowModal] = useState(false);
 
     const handleClick = () => {
         setShowModal(true)
-    }
+    };
 
-    return <div className="relative">
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
+    const actionBar = (<div>
+        <Button primary onClick={handleClose}>I accept</Button>
+    </div>);
+
+    const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+        <p>This is the children prop</p>
+    </Modal> ;
+
+    return <div>
         <Button primary onClick={handleClick}>Open Modal</Button>
-        {showModal && <Modal/>}
-    </div>
+        {showModal && modal}
+        </div>
 }
 
 export default ModalPage;
