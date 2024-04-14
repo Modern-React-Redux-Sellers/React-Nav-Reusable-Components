6e510969683b3132a4fdf3b9bb6fed58f22c69a2
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2024-04-13 20:13:43 -0500

    refactored counterpage using immer library

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,6 +1,7 @@
 import Button from "../components/Button";
 import {useReducer} from "react";
 import Panel from "../components/Panel";
+import {produce} from "immer";
 
 const INCREMENT_COUNT = 'increment-count';
 const CHANGE_VALUE_TO_ADD = 'change-value-to-add';
@@ -10,28 +11,20 @@ const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 const reducer = (state, action) => {
     switch (action.type) {
         case INCREMENT_COUNT:
-            return {
-                ...state,
-                count: state.count + 1
-            }
+           state.count = state.count + 1;
+           return;
         case CHANGE_VALUE_TO_ADD:
-            return {
-                ...state,
-                valueToAdd: action.payload
-            }
+            state.valueToAdd = action.payload;
+            return;
         case DECREMENT_COUNT:
-            return {
-                ...state,
-                count: state.count - 1
-            }
+           state.count = state.count - 1;
+           return;
         case ADD_VALUE_TO_COUNT:
-            return {
-                ...state,
-                count:state.count + state.valueToAdd,
-                valueToAdd:0
-            };
+            state.count = state.count + state.valueToAdd;
+            state.valueToAdd = 0;
+            return;
         default:
-            return state;
+            return;
     }
 
 }
@@ -39,7 +32,7 @@ const reducer = (state, action) => {
 const CounterPage = ({initialCount}) => {
     // const [count, setCount] = useState(0);
     // const [valueToAdd, setValueToAdd] = useState(0);
-    const [state, dispatch] = useReducer(reducer, {
+    const [state, dispatch] = useReducer(produce(reducer), {
         count: initialCount,
         valueToAdd: 0
     });
